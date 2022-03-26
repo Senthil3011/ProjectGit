cb94f8f0290edf14e32cd1ebff0a0bf9d79d8d7f
Senthil3011 <senthilkumarn3011@gmail.com>
2022-03-26 20:48:30 +0530

    Task

---
+++ src/org/prime/Prime.java
@@ -0,0 +1,35 @@
+package org.prime;
+
+public class Prime {
+	
+	public static void main(String[] args) {
+		
+		int num = 3;
+		int count = 0;
+		if (num>1)
+		{
+			for (int i = 1; i <=num; i++) {
+				if(num%i == 0) {
+					count++;
+				}
+					
+			}
+				if(count==2) {
+					System.out.println(num + " "+"is a prime number");
+				}
+				else
+				{
+					System.out.println(num + " "+"not a prime number");
+				}
+				
+			
+		}
+		else
+		{
+			System.out.println(num + " "+ " is not a prime number");
+		}
+	}
+
+}
+
+
